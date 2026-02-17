5fb5ab544351eb9707a688508607910fcc199b89
BistonN <biston.nunes@gmail.com>
2026-02-17 14:38:48 -0300

    refactor: Using R runtime to execute the model on API

--- api/routes/predict.py
+++ api/routes/predict.py
@@ -1,27 +1,31 @@
 import os
 import pandas as pd
-import pyreadr
 from flask import Blueprint, request, jsonify
+from rpy2 import robjects
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+from rpy2.robjects import default_converter
 
 predict_bp = Blueprint("predict", __name__)
 
-env = os.getenv("ENV", "prod")
+MODEL_PATH = os.getenv("MODEL_PATH", "/app/artifacts/model_v1.rds")
 
-if env == "dev":
-    model_path = "../../training/artifacts/model_v1.rds"
-else:
-    model_path = os.getenv("MODEL_PATH", "/app/artifacts/model_v1.rds")
+readRDS = robjects.r["readRDS"]
+predict_r = robjects.r["predict"]
 
-model_object = pyreadr.read_r(model_path)
-model = list(model_object.values())[0]
+model = readRDS(MODEL_PATH)
 
 @predict_bp.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
     df = pd.DataFrame([data])
 
-    probs = model.predict_proba(df)
-    prob_default = float(probs[0][1])
+    with localconverter(default_converter + pandas2ri.converter):
+        r_df = robjects.conversion.py2rpy(df)
+
+    probs = predict_r(model, r_df, type="prob")
+    prob_default = float(probs.rx2(".pred_1")[0])
+
     prediction = int(prob_default >= 0.5)
 
     return jsonify({

d65a90e5ca3efeeee580299301befa7c7e24b41f
BistonN <biston.nunes@gmail.com>
2026-02-17 14:58:52 -0300

    feat: Creating /model_info route
    Returns model informations

--- api/app.py
+++ api/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from routes.health import health_bp
 from routes.predict import predict_bp
+from routes.model_info import model_info_bp
+
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(health_bp)
     app.register_blueprint(predict_bp)
+    app.register_blueprint(model_info_bp)
 
     return app
 
